98aafddd01a77290ae05eb2d39f86b0da81d62be
Acyco <769225491@qq.com>
2021-01-17 05:43:19 +0800

    add log

---
+++ src/main/java/net/cherrymc/log/CherryLog.java
@@ -0,0 +1,15 @@
+package net.cherrymc.log;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * log
+ *
+ * @author Acyco
+ * @create 2021-01-17 02:51
+ * @url https://acyco.cn
+ */
+public class CherryLog {
+    public static final Logger log = LogManager.getLogger("Cherry");
+}
