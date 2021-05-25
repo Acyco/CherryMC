0bad59d3b449df74c41c7f7a9f85aa3b9c025262
Acyco <chencongyi@live.com>
2021-05-25 10:09:29 +0800

    refactor(Forge ID): Modify mod block and item starting ID from 4096 to 8192

--- src/main/java/net/minecraftforge/registries/GameData.java
+++ src/main/java/net/minecraftforge/registries/GameData.java
@@ -103,7 +103,7 @@ public class GameData
     public static final ResourceLocation PROFESSIONS  = new ResourceLocation("minecraft:villagerprofessions");
     public static final ResourceLocation SERIALIZERS  = new ResourceLocation("minecraft:dataserializers");
 
-    private static final int MAX_BLOCK_ID = 4095;
+    private static final int MAX_BLOCK_ID = 8191;
     private static final int MIN_ITEM_ID = MAX_BLOCK_ID + 1;
     private static final int MAX_ITEM_ID = 31999;
     private static final int MAX_POTION_ID = 255; // SPacketEntityEffect sends bytes, we can only use 255
